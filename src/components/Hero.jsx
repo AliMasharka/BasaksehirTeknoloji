d06fdf98f58d3289012978fdbea910640e19fb1e
Ali Mcharka <alimsrka323@gmail.com>
2024-08-06 18:03:47 +0300

    08062024

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           <h1 className="h1 mb-6">
             Takımımızın&nbsp;Yeteneklerini&nbsp;Web Sitemizle {` `}
             <span className="inline-block relative">
-              Keşfedin{" "}
+              Nigga{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
